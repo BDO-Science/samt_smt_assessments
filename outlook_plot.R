1cc680609fb6c2d69c065ba947e984d16a1d6c17
Pien <cpien@usbr.gov>
2025-11-07 10:48:36 -0800

    updated the date the outlook table starts tracking (wy start)

--- outlook_plot.R
+++ outlook_plot.R
@@ -1,39 +1,21 @@
----
-title: "outlook_plot"
-format: docx
-execute: 
-  echo: false
-  warning: false
-  message: false
-editor: visual
----
-
-# Outlook Plot
-
-```{r}
 library(lubridate)
 library(ggplot2)
 library(dplyr)
-library(readr)
 library(sharpshootR)
 library(here)
 library(CDECRetrieve)
-library(kableExtra)
-library(flextable)
-```
+library(readr)
 
-## Get data
+# One panel: Vernalis and Freeport flow
+# One panel: Clifton Court Inflow and Jones PP - DWR spreadsheet? 
+# One panel: Daily OMR (SacPAS) + triggers
 
-### Define start and end dates
-
-```{r}
-start <- "2025-10-01"
+# Get data -------------------------------
+# Define start and end dates - these will remain the same throughout
+start <- "2025-09-01"
 end <- today()
-```
 
-### Exports Data - not sure if this is the best source (CDEC)
-
-```{r}
+## Exports data (perhaps DWR has more up to date values though?) -------------
 stations_exp <- c("TRP", "HRO")
 pumping <- lapply(stations_exp, function(x) {cdec_query(station = x ,sensor_num = 70, dur_code = "D", start_date = start,end_date = end)})
 
@@ -45,22 +27,16 @@ pumping_clean <- pumping_df %>%
   filter(!is.na(value),
          !is.na(station)) %>%
   mutate(facility = if_else(station == "TRP", "CVP", "SWP"))
-```
 
-### OMRI - Daily value from SacPAS. Lots of missing data.
-
-```{r}
+## OMR -------------------
 url_omr <- "https://www.cbr.washington.edu/sacramento/data/php/rpt/mg.php?sc=1&mgconfig=river&outputFormat=csvSingle&hafilter=Delta&year%5B%5D=2025&loc%5B%5D=DTO&data%5B%5D=OMRIndex&tempUnit=F&startdate=1%2F1&enddate=12%2F31&avgyear=0&consolidate=1&grid=1&y1min=&y1max=&y2min=&y2max=&size=large"
 omr <- read_csv(url_omr)
 omr_clean <- omr %>%
   filter(!is.na(parameter)) %>%
   mutate(date = ymd(paste0(year, "-", `mm-dd`))) %>%
   filter(date < end, date >= start)
-```
 
-### Freeport and Vernalis Flow
-
-```{r}
+## Freeport and Vernalis -------------
 stations_flow <- c("FPT", "VNS")
 flow <- lapply(stations_flow, function(x) {cdec_query(station = x ,sensor_num = 20, dur_code = "H", start_date = start,end_date = end)})
 
@@ -75,22 +51,15 @@ flow_clean <- flow_df %>%
   group_by(date, station, parameter) %>%
   summarize(value = mean(value)) %>%
   ungroup()
-```
 
-### Triggers
-
-This file needs to be updated manually when an action is triggered
-
-```{r}
+## Triggers ---------------------
 triggers <- read_csv("data_raw/data_triggers.csv")
 triggers_clean <- triggers %>%
   mutate(date = mdy(date_triggered))
-```
 
-## Make Plot
+# Make plot ----------------------
+## Individual plots ---------------------------
 
-```{r}
-#| include: false
 export_plot <- pumping_clean %>%
   ggplot(aes(x = date)) +
   labs(y = 'Exports (cfs)') +
@@ -98,76 +67,40 @@ export_plot <- pumping_clean %>%
   scale_fill_manual(values = c('#0072B2', 'gray80')) +
   scale_x_date(date_breaks = '2 weeks', date_labels = '%b %d') +
   theme_bw() +
-  theme(axis.title.x = element_blank(),
-        axis.text = element_text(size = 15),
-        legend.text = element_text(size =15),
-        legend.title = element_text(size = 15),
-        legend.position = "bottom",
-        axis.title= element_text(size = 15))
+  theme(legend.position = 'bottom',
+        axis.title.x= element_blank())
 export_plot
-```
 
-```{r}
-#| include: false
 # Pull annotation start value for flow plot
 ann_start_FPT <- flow_clean %>% filter(station == "FPT", date == date(start)) %>% pull(value)
 ann_start_VNS <- flow_clean %>% filter(station == "VNS", date == date(start)) %>% pull(value)
 
 flow_plot <- flow_clean %>%
   ggplot(aes(x = date)) +
-  annotate("label", x = date(start)+2, y = ann_start_FPT-3000, label = "FPT", color = "#388715")+
+  annotate("label", x = date(start)+2, y = ann_start_FPT-3000, label = "FPT", color = "steelblue3")+
   annotate("label", x = date(start)+2, y = ann_start_VNS+3000, label = "VNS", color = "gray20")+
   labs(y = 'Flow (cfs)') +
   geom_line(aes(y = value, color = station)) +
-  scale_color_manual(values = c("#43a419", "gray15")) +
+  scale_color_manual(values = c("steelblue3", "gray15")) +
   scale_x_date(date_breaks = '2 weeks', date_labels = '%b %d') +
   theme_bw() +
-  theme(legend.position = "none",
-        axis.title.x = element_blank(),
-        axis.text = element_text(size = 15),
-        axis.title= element_text(size = 15))
+  theme(legend.position = 'none',
+        axis.title.x = element_blank())
 flow_plot
-```
 
-```{r}
-#| include: false
 omr_plot <- 
   ggplot() +
   geom_line(data = omr_clean, aes(x = date, y = value)) +
   geom_vline(data = triggers_clean, aes(xintercept = date, color = regulation))+
-  geom_text(data = triggers_clean, aes(x = date-2, y = -3500, label = plot_label), size = 5)+
+  geom_text(data = triggers_clean, aes(x = date-2, y = -3500, label = plot_label))+
   labs(y = 'Daily OMRI (cfs)') +
   scale_x_date(date_breaks = '2 weeks', date_labels = '%b %d') +
   scale_color_manual(values = c("magenta4", "blue3"))+
   theme_bw() +
   theme(legend.position = "none",
-        axis.title.x = element_blank(),
-        axis.text = element_text(size = 15),
-        axis.title = element_text(size = 15))
+        axis.title.x = element_blank())
 omr_plot
-```
 
-```{r}
-#| label: fig-triggers
-#| fig-cap: "Operations and Action Summary, WY 2026. The colors in the OMRI plot indicate different types of triggers (see Table 1). OMRI data calculated by SacPAS, Freeport (FPT) and Vernalis (VNS) flow data from CDEC, and CVP (TRP) and SWP (HRO) exports data from CDEC."
-#| fig-width: 9
-#| fig-height: 7
- 
+## Combine plots -------------------
 library(patchwork)
-flow_plot +omr_plot + export_plot + plot_layout(nrow = 3)
-```
-
-```{r}
-#| label: tbl-triggers
-#| tbl-cap: "Summary of Actions and Triggers, WY 2026"
-
-trigger_table <- triggers_clean %>%
-  select(Label = plot_label,
-         Action = action,
-         `Date Triggered` = date_triggered,
-         `Date Implemented` = date_implemented,
-         `Number Days Implemented` = days_implemented,
-         Regulation = regulation,
-         )
-kable(trigger_table)
-```
+omr_plot + flow_plot +export_plot +  plot_layout(nrow = 3)
