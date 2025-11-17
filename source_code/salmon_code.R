c9ce085fd2784d09ec26ce36dffb727e7f50f13f
Ehlo <cehlo@usbr.gov>
2025-11-17 15:19:56 -0800

    samt updates

--- source_code/salmon_code.R
+++ source_code/salmon_code.R
@@ -1,117 +1,145 @@
 library(tidyverse)
 library(rvest)
-library(plotly)
 library(janitor)
+library(busdater)
 
 wy <- 2026
 url <- 'https://filelib.wildlife.ca.gov/Public/salvage/Salmon%20Monitoring%20Team%20and%20Sturgeon/' #site with salvage files
 season_start <- ymd(paste0(wy-1,'-10-01'))
 season_end <- ymd(paste0(wy,'-06-30'))
+jpe <- NA
+livingston_jpe <- NA
+battle_jpe <- NA
 ##########################################
 #pull in latest salmon and steelhead files
 ##########################################
 
-#isolating csv files on CDFW ftp site
-url_root <- 'https://filelib.wildlife.ca.gov'
-page <- read_html(url) #read url in
-links <- page %>% html_nodes("a") %>% html_attr("href") # Extract all links from the webpage
+# ###CDFW method
+# #isolating csv files on CDFW ftp site
+# url_root <- 'https://filelib.wildlife.ca.gov'
+# page <- read_html(url) #read url in
+# links <- page %>% html_nodes("a") %>% html_attr("href") # Extract all links from the webpage
+# 
+# salmon_links <- grep(paste0("Salmon_",wy), links, value = TRUE, ignore.case = TRUE)
+# salmon_file <- max(grep("\\.csv$", salmon_links, value = TRUE, ignore.case = TRUE))
+# 
+# steelhead_links <- grep(paste0('Steelhead_Salvage_Summary_', wy), links, value = TRUE, ignore.case = TRUE)
+# steelhead_file <- max(grep("\\.csv$", steelhead_links, value = TRUE, ignore.case = TRUE))
+# 
+# #reading csv files
+# salmon <- tryCatch(
+#   read_csv(paste0(url_root, salmon_file)),
+#   error = function(e) NULL
+# )
+# steelhead <- tryCatch(read_csv(paste0(url_root, steelhead_file)),
+#                       error = function(e) NULL
+#                       )
+###SacPAS urls
+# Read once and store
+# salmon_raw <- read_csv(paste0('https://www.cbr.washington.edu/sacramento/data/php/rpt/juv_loss_detail.php?sc=1&outputFormat=csv&year=',
+#                               wy, '&species=1%3Aall&dnaOnly=no&age=no')) %>%
+#   clean_names()
+# 
+# # Check and assign
+# salmon <- if(ncol(salmon_raw) == 1) {
+#   NULL
+# } else {
+#   salmon_raw
+# }
+# 
+# steelhead_raw <- read_csv(paste0('https://www.cbr.washington.edu/sacramento/data/php/rpt/juv_loss_detail.php?sc=1&outputFormat=csv&year='
+#                                  ,wy,'&species=2%3Aall&dnaOnly=no&age=no')) %>%
+#   clean_names()
+# steelhead <- if(ncol(steelhead_raw) == 1) {
+#   NULL
+# } else {
+#   steelhead_raw
+# }
 
-salmon_links <- grep(paste0("Salmon_",wy), links, value = TRUE, ignore.case = TRUE)
-salmon_file <- max(grep("\\.csv$", salmon_links, value = TRUE, ignore.case = TRUE))
-
-steelhead_links <- grep(paste0('Steelhead_Salvage_Summary_', wy), links, value = TRUE, ignore.case = TRUE)
-steelhead_file <- max(grep("\\.csv$", steelhead_links, value = TRUE, ignore.case = TRUE))
-
-#reading csv files
-salmon <- tryCatch(
-  read_csv(paste0(url_root, salmon_file)),
-  error = function(e) NULL
-)
-steelhead <- tryCatch(read_csv(paste0(url_root, steelhead_file)),
-                      error = function(e) NULL
-                      )
-
-
+###SacPAS table
+loss_summary_url <- paste0('https://www.cbr.washington.edu/sacramento/workgroups/include_gen/WY',wy,
+                           '/LossUnclip_WY_summary.html')
+loss_summary <- read_html(loss_summary_url) %>% 
+  html_nodes("table") %>%
+  html_table(fill = T) 
+loss_summary_table <- loss_summary[[1]] %>%
+  clean_names()
 ###########################################
 #summarize data for report and graphs
 ###########################################
 dates <- data.frame(Date = seq(season_start, season_end, by = '1 days'))
+
 #natural winter-run
-# wr_loss <- salmon %>% 
-#   filter(AdClip == 'N',
-#          Size_Run == 'W' & (DNA_Run == 'W' | is.na(DNA_Run))) %>%
-#   pivot_wider(names_from = 'BuildingCode', values_from = 'LOSS') %>%
-#   right_join(dates, by = c('SampleDate' = 'Date')) %>%
-#   mutate(Loss = if_else(is.na(Loss), 0, LOSS)) %>%
-#   arrange(SampleDate) %>%
-#   mutate(cumul_loss = cumsum(LOSS)) %>%
-#   mutate(text = paste(
-#     "<span style='font-size:16px;'><b>", SampleDate, "</b></span><br>",
-#     "<br><b>Daily Loss (M):</b> ", pop,
-#     "<br><b>Life Expectancy (years):</b> ", round(lifeExp, 2),
-#     "<br><b>Gdp per capita:</b> $", round(gdpPercap, 2),
-#     sep=""
-#   ))
-
+wr_loss <- loss_summary_table %>%
+  select(dna_winter_run_chinook) %>%
+  slice(3) %>%
+  pull()
+wr_perc <- loss_summary_table %>%
+  select(dna_winter_run_chinook) %>%
+  slice(4) %>%
+  pull()
+wr_7d <- loss_summary_table %>%
+  select(dna_winter_run_chinook) %>%
+  slice(1) %>%
+  pull()
 #hatchery winter-run
-wr_hatch_loss <- tryCatch(salmon %>%
-  filter(AdClip == 'C',
-         CWT_Run == 'W') %>%
-  pivot_wider(names_from = 'BuildingCode', values_from = 'LOSS') %>%
-  right_join(dates, by = c('SampleDate' = 'Date')) %>%
-  mutate(across((ncol(.) - 1):ncol(.), ~replace_na(.x, 0))) %>%
-  mutate(Loss = F + NS) %>%
-  mutate(Loss = if_else(is.na(Loss), 0, Loss)) %>%
-  arrange(SampleDate) %>%
-  mutate(cumul_loss = cumsum(Loss)) %>%
-  mutate(text = paste(
-    "<span style='font-size:16px;'><b>", SampleDate, "</b></span><br>",
-    "<br><b>Cumulative Loss:</b> ", cumul_loss,
-    "<br><b>Total Daily Loss:</b> ", Loss,
-    "<br><b>CVP Daily Loss :</b> ", F,
-    "<br><b>SWP Daily Loss", NS,
-    sep="")),
-  error = function(e) NULL)
-
-# hatch_facility_loss <- salmon %>%
-#   filter(AdClip == 'C',
-#          CWT_Run == 'W') %>%
-#   mutate(BuildingCode = factor(BuildingCode, levels = c('F', 'NS'),
-#                                labels = c('CVP', 'SWP')))
-# 
-# hatch_graph <- ggplot() +
-#   geom_line(wr_hatch_loss, mapping = aes(x = SampleDate, y = cumul_loss, group = 1, text = text)) +
-#   geom_col(hatch_facility_loss, 
-#            mapping = aes(x = SampleDate, y = LOSS, fill = BuildingCode)) +
-#   labs(x = 'Date', y = 'Cumulative Loss', fill = 'Facility') +
-#   scale_fill_manual(values = c("#009E73", "#D55E00")) +
-#   xlim(min_date, season_end) +
-#   theme_bw() +
-#   theme(legend.position = 'bottom',
-#         plot.margin = margin(0.2, 0.5, 0.2, 0.2, unit = 'cm'),
-#         axis.title.y = element_text(margin = margin(r = 15), size = 15),
-#         axis.title.x = element_text(margin = margin(t = 15), size = 15),
-#         strip.text = element_text(size = 13),
-#         axis.text.x = element_text(size = 13),  # Adjust angle
-#         axis.text.y = element_text(size = 13),)
-# hatch_graph
-# 
-# hatch_graph_interact <- ggplotly(hatch_graph, tooltip="text") %>%
-#   layout(legend = list(orientation = "h",   # horizontal
-#                        x = 0.5, y = -0.2,   # position
-#                        xanchor = "center",
-#                        yanchor = "top"))
-# hatch_graph_interact
-#steelhead
+# wr_hatch_loss <- if(is.null(salmon)) {
+#   print(0)
+# } else {
+#   salmon %>% filter(cwt_race == 'Winter') %>%
+#     summarize(loss = sum(loss, na.rm = TRUE)) %>%
+#     pull()
+# }
 
 
 #hatchery steelhead
+wr_hatch <- read_csv('https://www.cbr.washington.edu/sacramento/workgroups/include_gen/WY2026/cwt_winter_releases.csv') %>%
+  clean_names() %>%
+  mutate(wYear = get_fy(as.Date(release_start), opt_fy_start = '10-01')) %>%
+  filter(wYear == wy) %>%
+  mutate(loss = ifelse(is.na(loss), 0, loss))
+
+liv_loss <- wr_hatch %>%
+  filter(grepl('livingston', hatchery, ignore.case = TRUE)) %>%
+  summarize(loss = sum(loss)) %>%
+  pull()
+
+batt_loss <- wr_hatch %>%
+  filter(grepl('coleman', hatchery, ignore.case = TRUE)) %>%
+  summarize(loss = sum(loss)) %>%
+  pull()
+
+liv_perc <- if(is.na(liv_loss/(livingston_jpe * 0.0017))) {
+  print('0%')
+} else {
+  paste0(round((liv_loss/(livingston_jpe*0.0017))*100,2),'%')
+}
+
+batt_perc <- if(is.na(batt_loss/(battle_jpe * 0.0017))) {
+  print('0%')
+} else {
+  paste0(round((batt_loss/(battle_jpe*0.0017))*100,2),'%')
+}
+
+#natural steelhead
+sh_loss <- loss_summary_table %>%
+  select(natural_steelhead) %>%
+  slice(3) %>%
+  pull()
+sh_perc <- loss_summary_table %>%
+  select(natural_steelhead) %>%
+  slice(4) %>%
+  pull()
+sh_7d <- loss_summary_table %>%
+  select(natural_steelhead) %>%
+  slice(1) %>%
+  pull()
 
 ###########################################
 #pull in migration timing table
 ###########################################
 
-timing_url <- 'https://www.cbr.washington.edu/sacramento/workgroups/include_gen/WY2026/samt_hrt.html'
+timing_url <- paste0('https://www.cbr.washington.edu/sacramento/workgroups/include_gen/WY',wy,'/samt_hrt.html')
 timing <- read_html(timing_url) %>% 
   html_nodes("table") %>%
   html_table(fill = T) 
@@ -147,3 +175,26 @@ delta_exit_sh <- sh_natural_timing[1,6] %>%
   pull()
 salvage_sh <- sh_natural_timing[1,7] %>%
   pull()
+
+
+#######################################
+#STARs table
+#######################################
+
+stars_url <- paste0('https://www.cbr.washington.edu/sacramento/workgroups/include_gen/WY',wy,'/samt_stars.html')
+
+stars <- read_html(stars_url) %>% 
+  html_nodes("table") %>%
+  html_table(fill = T) 
+stars_table <- stars[[1]] %>%
+  filter(Stock == 'Winter Chinook')
+stars_date <- min(stars_table$Date)
+overall_survival <- paste0(round(stars_table[2,5],2)," (",
+                           round(stars_table[2,6],2),"-",
+                           round(stars_table[2,7],2),")")
+id_survival <- paste0(round(stars_table[17,5],2)," (",
+                      round(stars_table[17,6],2),"-",
+                      round(stars_table[17,7],2),")")
+id_routing <- paste0(round(stars_table[16,5],2)," (",
+                     round(stars_table[16,6],2),"-",
+                     round(stars_table[16,7],2),")")
--- source_code/text_updates.R
+++ source_code/text_updates.R
@@ -16,12 +16,62 @@ early_season_text <- if(Sys.Date() < as.Date(paste0(wy-1,"-11-01")) | Sys.Date()
 #####################################################
 ##setting language for annual loss thresholds
 #####################################################
-wr_annual_text <- if(is.null(salmon) & is.null(steelhead)) {
-  print(paste0('No loss of natural winter-run salmon or steelhead has occurred in WY ',wy))
+wr_annual_text <- print(paste0('As of ',format(Sys.Date()-1, '%B %d'),' cumulative loss of genetically confirmed winter-run is ', wr_loss,
+                               ' or ',wr_perc,' of the annual loss threshold. Cumulative loss in the past 7 days has been ',
+                               wr_7d, '.'))
+sh_annual_text <- print(paste0('As of ',format(Sys.Date()-1, '%B %d'),' cumulative loss of unclipped steelhead is ', sh_loss,
+                               ' or ',sh_perc,' of the annual loss threshold. Cumulative loss in the past 7 days has been ',
+                               sh_7d, '.'))
+
+wr_jpe <- if(is.na(jpe)) {
+  print('The Juvenile Production Estimate for winter-run has not been established for the current water year.')
 } else {
-  print('Loss has occurred')
+  print(paste0('The Juvenile Production Estimate for winter-run is ',jpe,' for the current water year'))
 }
 
+wr_hatch_jpe <- if(is.na(livingston_jpe)) {
+  print('The Juvenile Production Estimate for Livingstone Stone and Battle Creek hatchery winter-run has not been established for the current water year.')
+} else {
+  print(paste0('The Juvenile Production Estimate for hatchery winter-run is '
+         ,livingston_jpe,' for Livingston Stone releases and '
+         ,battle_jpe,' for Battle Creek releases in the current water year'))
+}
+
+wr_threshold <- if(is.na(jpe)) {
+  print('The Juvenile Production Estimate has not been established for the current water year so thresholds are absent or are included from the previous water year.')
+} else {
+  print(paste0('The annual Loss threshold for natural winter-run is 0.5% of the jpe or ', round(jpe*.005,2),'fish.'))
+}
+
+wr_hatch_threshold <- if(is.na(livingston_jpe)) {
+  print('The Juvenile Production Estimate has not been established for the current water year so thresholds are absent or are included from the previous water year.')
+} else {
+  print(paste0('The annual Loss threshold for Livingston Stone and Battle Creek hatchery winter-run releases is 0.17% of the jpe for each release or ', 
+               round(livingston_jpe*.0017,2),' and ', round(battle_jpe*0.0017,2), ' fish for Livingston and Battle Creek respectively.'))
+}
+  
+wr_hatchery_releases <- if(nrow(wr_hatch) == 0) {
+   print(paste0('To date, no winter-run hatchery releases have occurred in WY ',wy))
+  } else {
+   if(nrow(wr_hatch) == 1) {
+     print(paste0('A total of xx fish were released from Livingston Stone National Fish Hatchery on xx'))
+   } else{
+     print(paste0('A total of xx and xx fish were released from Livingston Stone National Fish Hatchery and Battle Creek on xx and xx respectively.'))
+   }
+  }
+
+wr_hatchery_loss <- if(nrow(wr_hatch) == 0) {
+  print('To date, no loss has occurred as no hatchery winter-run have been released.')
+} else {
+  if(nrow(wr_hatch) == 1) {
+    print(paste0('As of ',format(Sys.Date(), '%B %d'), ' cumulative loss of Livingston Stone hatchery fish is ', 
+               liv_loss, ' or ', liv_perc, ' of the annual loss threshold. No fish have been released from Battle Creek Hatchery.'))
+  } else {
+    print(paste0('As of ',format(Sys.Date(), '%B %d'), ' cumulative loss of Livingston Stone hatchery fish is ', 
+                 liv_loss, ' or ', liv_perc, ' of the annual loss threshold. Cumulative loss of Battle Creek hatchery fish is ',
+                 batt_loss, ' or ', batt_perc, ' of the annual loss threshold'))
+  }
+}
 #####################################################
 ##setting language for weekly loss thresholds
 #####################################################
@@ -30,3 +80,11 @@ weekly_loss_text <- if(Sys.Date() < as.Date(paste0(wy,"-1-01"))) {
 } else {
   print(paste0('As of ',Sys.Date(),'7-day rolling some of salmon and steelhead has occurred.'))
 }
+
+#####################################################
+##setting language for STARs estimate
+#####################################################
+stars_text <- print(paste0("As of ",format(as.Date(stars_date), '%B %d'), 
+                           ' overall through delta STARs estimated survival probability (with 80% credible intervals) is ',overall_survival,
+                           '. STARs estimated rounting and survival probabilities (with 80% credible intervals) into the interior delta are',
+                           id_routing,' and ', id_survival, ' respectively.'))
