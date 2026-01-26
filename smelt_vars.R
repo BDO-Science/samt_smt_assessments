3787be84f7a673735d48c95645bd814aef8482d1
basket-grace <geasterbrook@usbr.gov>
2026-01-26 14:39:39 -0800

    fixed a date in SLS turbidity

--- smelt_vars.R
+++ smelt_vars.R
@@ -128,8 +128,8 @@ ds_recent_display <- ds_recent %>%
 
 ## EDIT: South Delta conditions (turbidity and secchi depth-currently by email)-------------------------
 sd_secchi_depth <- 0.76
-sd_secchi_date <- ymd("2026-05-01")
-sd_turb <- 12.5
+sd_secchi_date <- ymd("2026-01-13")
+sd_turb <- 12.2
 sd_turb_date <- ymd("2025-01-13")
 
 ## Abundance ------------------------------
